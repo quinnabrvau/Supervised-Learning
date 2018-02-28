4876ede0d7a23e99da67b65fe89fe53935883105
Quinn Abrahams-Vaughn <quinna@wearstrive.com>
2018-02-28 12:03:07 -0800

    use the same data read in commands

--- NNTestDriver.py
+++ NNTestDriver.py
@@ -7,8 +7,8 @@ Final Project
 Test Driver to use in testing our learning agents
 """
 
-import csv
 import numpy as np
+from dataSet import dataSet, getIrisData, getWineData
 from NeuralNet import NeuralNet
 
 
@@ -30,31 +30,27 @@ def calculate_accuracy(Y, Y_predict):
 
 
 if __name__ == "__main__":
-
-    # read the csv files and put wine data into numpy ndarrays
-    train_reader = csv.reader(open('wineTrainData.csv'))
-    test_reader = csv.reader(open('wineTestData.csv'))
-    valid_reader = csv.reader(open('wineValidData.csv'))
-
-    train_data = np.asarray(list(train_reader))
-    test_data = np.asarray(list(test_reader))
-    valid_data = np.asarray(list(valid_reader))
+    train,test,valid = getIrisData(1)
+    train,test,valid = getWineData(1)
+    train_data = np.asarray(train)
+    test_data = np.asarray(test)
+    valid_data = np.asarray(valid)
 
     # Create numpy ndarray matrices of samples and their attributes (as floats)
     # leaving off the first row with the column names
     # and the last two columns, which hold the classification and an empty row
-    X_train = train_data[1:, :-2]
+    X_train = train_data[1:, :-1]
     X_train = X_train.astype(float)
-    X_test = test_data[1:, :-2]
+    X_test = test_data[1:, :-1]
     X_test = X_test.astype(float)
-    X_valid = valid_data[1:, :-2]
+    X_valid = valid_data[1:, :-1]
     X_valid = X_valid.astype(float)
 
     # Create numpy ndarray vectors for the classifications for the samples
     # with 0 for red, and 1 for white
-    Y_train = np.asarray([int(row[-2] == 'w') for row in train_data[1:]])
-    Y_test = np.asarray([int(row[-2] == 'w') for row in test_data[1:]])
-    Y_valid = np.asarray([int(row[-2] == 'w') for row in valid_data[1:]])
+    Y_train = np.asarray([int(row[-1] == 'w') for row in train_data[1:]])
+    Y_test = np.asarray([int(row[-1] == 'w') for row in test_data[1:]])
+    Y_valid = np.asarray([int(row[-1] == 'w') for row in valid_data[1:]])
 
     # Create a new neural net
     num_train_samples, num_features = X_train.shape
