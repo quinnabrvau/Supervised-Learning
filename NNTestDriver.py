9cca2272a5a541a43665b03b453447faf681b939
Shannon Ladymon <sladymon@gmail.com>
2018-03-02 17:56:57 -0800

    Got a temporary fix for NNTestDriver by hard-coding classification size

--- NNTestDriver.py
+++ NNTestDriver.py
@@ -13,6 +13,7 @@ from NeuralNet import NeuralNet
 import time
 
 
+
 def calculate_accuracy(Y, Y_predict):  # TODO: Break into a separate Report class
     """Returns the percentage of accurate predictions"""
     total_entries = Y.shape[0]
@@ -44,12 +45,7 @@ class NNTestDriver:
         num_train_samples, num_features = self.X_train.shape
         alpha = 10e-5  # learning rate
         num_classifications = len(train.classes)
-        print("TESTING: num_classifications", num_classifications)
-        self.NN = NeuralNet(num_features, num_classifications, alpha)
-
-
-        # alpha = 10e-5  # learning rate
-        # self.NN = NeuralNet(len(train[0])-1, len(train.classes), alpha)
+        self.NN = NeuralNet(num_features, num_classifications + 3, alpha)  # TODO: Fix the num_classi
 
     def build(self):
         start_time = time.time()  # TODO: Possibly format time?
@@ -63,70 +59,15 @@ class NNTestDriver:
         Y_test_predict, test_loss = self.NN.predict(self.X_test, self.Y_test)
         return Y_test_predict
 
-
+    def params(self):
+        return None
 
 
 if __name__ == "__main__":
-
     # Get training, testing, and validation data
-    # and put into ndarray format
-
-    # train, test, valid = getIrisData(1)
-    # num_classifications = 3  # TODO: Fix this to call from data (add dataSet method?)
-
     train, test, valid = getWineData(1)
+
 
     searcher = NNTestDriver(train,test,valid)
     searcher.build()
-    print(searcher(test))
-    # num_classifications = 13
-
-    # train_data = np.asarray(train)
-    # test_data = np.asarray(test)
-    # valid_data = np.asarray(valid)
-
-    # # Create the samples ndarray matrices with only the columns for the features
-    # # (leaving off the last column, which holds the classifications)
-    # X_train = train_data[:, :-1]
-    # X_test = test_data[:, :-1]
-    # X_valid = valid_data[:, :-1]
-
-    # # Create the classification ndarray vectors from the last column of the data
-    # Y_train = (train_data[:, -1]).astype(int)
-    # Y_test = (test_data[:, -1]).astype(int)
-    # Y_valid = (valid_data[:, -1]).astype(int)
-
-    # # Create a new neural net
-    # num_train_samples, num_features = X_train.shape
-    # alpha = 10e-5  # learning rate
-    # nn = NeuralNet(num_features, num_classifications, alpha)
-
-    # # Train the neural net on the training data
-    # start_time = time.time()  # TODO: Possibly format time?
-    # print("Starting Neural Net Training at time = ", start_time)
-    # nn.train(X_train, Y_train, X_valid, Y_valid)
-    # stop_time = time.time()
-    # total_time = stop_time - start_time
-    # print("Neural Net Training finished at time = ", stop_time, ", taking a total of ", total_time, " seconds")
-
-    # # Use the test data to predict classes to see how well the neural net classifies
-    # Y_train_predict, train_loss = nn.predict(X_train, Y_train)
-    # Y_test_predict, test_loss = nn.predict(X_test, Y_test)
-
-    # # TODO: Clean up after testing
-    # # print(Y_train)
-    # # print(Y_train_predict)
-    # print("Neural Net Accuracy for Training Data: ", calculate_accuracy(Y_train, Y_train_predict))
-
-    # # print(Y_test)
-    # print(Y_test_predict)
-    # print("Neural Net Accuracy for Test Data: ", calculate_accuracy(Y_test, Y_test_predict))
-
-
-
-
-
-
-
-
-
+    print(searcher.test())
--- NeuralNet.py
+++ NeuralNet.py
@@ -43,7 +43,6 @@ class NeuralNet:
         # where each row represents a sample, and each column represents a classification
         # and each row gets a 1 in the column that matches its classification
         one_hot_encodings = np.zeros((Y_train.shape[0], self.num_classifications))  # initialize to all zeros
-        print("TESTING: Y_train_shape:", Y_train.shape)
         for i in range(Y_train.shape[0]):
             one_hot_encodings[i, Y_train[i]] = 1
 
